7f5c5a8d744e108ea148b34ff00b438926ba40de
ThomasByr <tbouyer2000@gmail.com>
2023-07-06 16:47:28 +0200

    src

---
+++ src/helper/fmt.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import logging
+from dotenv import load_dotenv
+
+from .formatter import *
+
+__all__ = ['logger']
+
+# we load the env variables here so that we can use them in the logger
+# because this file may be imported in the main file first,
+# we can't load the env variables in the main file
+load_dotenv()
+DEBUG = os.getenv('DEBUG', 'False').lower() in {'true', '1', 'yes'}
+
+logger = logging.getLogger('me 🙂')
+logger.setLevel(log_lvl := logging.DEBUG if DEBUG else logging.INFO)
+
+# create console handler with a higher log level
+console_handler = logging.StreamHandler(sys.stderr)
+console_handler.setLevel(log_lvl)
+console_handler.setFormatter(default_formatter := UsefulFormatter())
+
+logger.addHandler(console_handler)
