8443a47ee2512ad83d7c1c6606f1dee17d4bf70f
ThomasByr <tbouyer2000@gmail.com>
2023-08-28 18:00:08 +0200

    src

--- src/helper/fmt.py
+++ src/helper/fmt.py
@@ -1,25 +1,42 @@
-import os
-import sys
-
 import logging
-from dotenv import load_dotenv
+from typing import Any
+from typing_extensions import override
+from termcolor import colored
 
-from .formatter import *
+__all__ = ['UsefulFormatter']
 
-__all__ = ['logger']
 
-# we load the env variables here so that we can use them in the logger
-# because this file may be imported in the main file first,
-# we can't load the env variables in the main file
-load_dotenv()
-DEBUG = os.getenv('DEBUG', 'False').lower() in {'true', '1', 'yes'}
+def formatter(
+  c: str,
+  colored_output: bool = True,
+  name_width: int = 10,
+  attrs: list[str] = None,
+) -> str:
+  if colored_output:
+    return f"{colored('%(asctime)s', 'grey', attrs=['bold'])} {colored('%(levelname)8s', c, attrs=attrs)}"\
+           f"{colored(f'%(name){name_width}s', 'magenta')} %(message)s"
+  return f'%(asctime)s %(levelname)8s %(name){name_width}s %(message)s'
 
-logger = logging.getLogger('me 🙂')
-logger.setLevel(log_lvl := logging.DEBUG if DEBUG else logging.INFO)
 
-# create console handler with a higher log level
-console_handler = logging.StreamHandler(sys.stderr)
-console_handler.setLevel(log_lvl)
-console_handler.setFormatter(default_formatter := UsefulFormatter())
+class UsefulFormatter(logging.Formatter):
 
-logger.addHandler(console_handler)
+  name_width = 4
+  dt_fmt = '%Y-%m-%d %H:%M:%S'
+
+  def __init__(self, *args: Any, colored_output: bool = True, **kwargs: Any) -> None:
+    super().__init__(*args, **kwargs)
+    self.colored_output = colored_output
+
+  @override
+  def format(self, record: logging.LogRecord) -> str:
+    self.name_width = max(len(record.name) + 1, self.name_width)
+    formats = {
+      logging.DEBUG: formatter('green', self.colored_output, self.name_width),
+      logging.INFO: formatter('blue', self.colored_output, self.name_width),
+      logging.WARNING: formatter('yellow', self.colored_output, self.name_width),
+      logging.ERROR: formatter('red', self.colored_output, self.name_width),
+      logging.CRITICAL: formatter('red', self.colored_output, self.name_width, ['bold']),
+    }
+    log_fmt = formats.get(record.levelno)
+    fmt = logging.Formatter(log_fmt, self.dt_fmt, style='%')
+    return fmt.format(record)
